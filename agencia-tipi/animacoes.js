0a29f7e76458eb6a1030573c12bb5e2dd950e24f
Kellinton <kellintonbrasil@gmail.com>
2023-06-13 17:04:18 -0300

    menu

--- agencia-tipi/animacoes.js
+++ agencia-tipi/animacoes.js
@@ -45,3 +45,13 @@ $('.responsive').slick({
       // instead of a settings object
     ]
   });
+
+  // Menu mobile
+
+  document.querySelector ('.abrir-menu').onclick = function () {
+    document.documentElement.classList.add('menu-ativo');
+  }
+
+  document.querySelector ('.fechar-menu').onclick = function () {
+    document.documentElement.classList.remove('menu-ativo');
+  }
